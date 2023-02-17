65b547f86b2ff370a2495ff02c30720f2c0b6247
“saka-git <sakamoto.tatsuya1997@gmail.com>
2023-02-17 22:45:59 +0900

    last

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,59 +1,32 @@
-<!DOCTYPE html>
-<html lang="ja">
+@extends('layouts.app')
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>新規投稿</title>
-</head>
+@section('title', '新規投稿')
 
-<body>
-  <header>
-    <nav>
-      <div>
-        <a href="{{ route('posts.index') }}">投稿アプリ</a>
-      </div>
-    </nav>
-  </header>
+@section('content')
+@if ($errors->any())
+<div class="alert alert-danger">
+  <ul>
+    @foreach ($errors->all() as $error)
+    <li>{{ $error }}</li>
+    @endforeach
+  </ul>
+</div>
+@endif
 
-  <main>
-    <article>
-      <div>
-        <h1>新規投稿</h1>
+<div class="mb-2">
+  <a href="{{ route('posts.index') }}" class="text-decoration-none">&lt; 戻る</a>
+</div>
 
-        @if ($errors->any())
-        <div>
-          <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-          </ul>
-        </div>
-        @endif
-
-        <div>
-          <a href="{{ route('posts.index') }}">&lt; 戻る</a>
-        </div>
-
-        <form action="{{ route('posts.store') }}" method="post">
-          @csrf
-          <div>
-            <label for="title">タイトル</label>
-            <input type="text" name="title" value="{{ old('title') }}">
-          </div>
-          <div>
-            <label for="content">本文</label>
-            <textarea name="content">{{ old('content') }}</textarea>
-          </div>
-          <button type="submit">投稿</button>
-        </form>
-      </div>
-    </article>
-  </main>
-
-  <footer>
-    <p>&copy; 投稿アプリ All rights reserved.</p>
-  </footer>
-</body>
-
-</html>
+<form action="{{ route('posts.store') }}" method="post">
+  @csrf
+  <div class="form-group mb-3">
+    <label for="title">タイトル</label>
+    <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+  </div>
+  <div class="form-group mb-3">
+    <label for="content">本文</label>
+    <textarea class="form-control" name="content">{{ old('content') }}</textarea>
+  </div>
+  <button type="submit" class="btn btn-outline-primary">投稿</button>
+</form>
+@endsection
